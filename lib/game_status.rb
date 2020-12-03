03d4c46b61ce4999339c81f7dbae7cadfbca3040
Shaina C <scailles23@gmail.com>
2020-12-03 02:56:53 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -68,5 +68,18 @@ def over?(board)
   end
 end
   
-def winner?(board)
+def winner (board)
+  index = []
+  index = won?(board)
+  if index == false
+    return nil
+  else
+    if board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
+  end
+end
+
   
